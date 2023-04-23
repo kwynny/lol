e3c0bfac8a92c1d69eb5a888abf14bf5d0d8d81a
kwynny <129724108+kwynny@users.noreply.github.com>
2023-04-23 10:52:53 -0700

    Update unobfuscated rolling.lua

--- unobfuscated rolling.lua
+++ unobfuscated rolling.lua
@@ -206,6 +206,7 @@ game:GetService("ReplicatedStorage").Events.GetStats:InvokeServer(ohTable1)
 
             game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Gui"):WaitForChild("Ui"):WaitForChild("UiModule"):WaitForChild("Modules"):WaitForChild("Settings"):WaitForChild("Set"):InvokeServer(unpack(args))
             game:GetService('TeleportService'):Teleport(game.PlaceId, game.Players.LocalPlayer)
+			wait(2)
         end
     end
     
@@ -218,7 +219,7 @@ game:GetService("ReplicatedStorage").Events.GetStats:InvokeServer(ohTable1)
         }
 
         game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Gui"):WaitForChild("Ui"):WaitForChild("UiModule"):WaitForChild("Modules"):WaitForChild("Settings"):WaitForChild("Set"):InvokeServer(unpack(args))
-		wait(2)
         game:GetService('TeleportService'):Teleport(game.PlaceId, game.Players.LocalPlayer)
+		wait(2)
     end
 end
